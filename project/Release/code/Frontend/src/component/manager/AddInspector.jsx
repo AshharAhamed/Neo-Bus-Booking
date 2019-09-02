eebbd748023d26a4c899d2fbecd3af293bf0c0b7
sathiraguruge <33770377+sathiraguruge@users.noreply.github.com>
2019-09-02 09:56:56 +0530

    UI Improvements

--- project/Release/code/Frontend/src/component/manager/AddInspector.jsx
+++ project/Release/code/Frontend/src/component/manager/AddInspector.jsx
@@ -65,56 +65,58 @@ export default class RegisterPassengerContainer extends Component {
     render() {
         return <div>
             <div>
-                <div>
+                <div className="float-sm-left">
                     <ul className="navbar-nav mr-auto">
                         <div className="navbar-brand"><Link className="navbar-brand" to="/manageInspectors"><i
                             className="fa fa-arrow-circle-left" style={{margin: '12px'}}/>Back
                         </Link></div>
                     </ul>
                 </div>
+                <br/><br/><br/><br/>
                 <QueueAnim>
                     <div key="1">
-                        <form onSubmit={this.onSubmit}>
-				<span className="contact100-form-title">
+                        <form onSubmit={this.onSubmit} style={{marginLeft: "50px"}}>
+				<span className="contact100-form-title float-lg-left">
 					Add Inspector
-				</span>
+				</span><br/><br/><br/><br/>
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="text-left">First Name</span>
+                                <span className="label-input100 float-left">First Name</span>
+                                <br/>
                                 <input className="input100" type="text" required={true} value={this.state.FirstName}
                                        onChange={this.onChange} name="FirstName" placeholder="Daniel"/>
                                 <span className="focus-input100"/>
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="label-input100">Last Name</span>
+                                <span className="label-input100  float-left">Last Name</span>
                                 <input className="input100" type="text" required={true} value={this.state.LastName}
                                        onChange={this.onChange} name="LastName" placeholder="Asplund"/>
                                 <span className="focus-input100"/>
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="label-input100">Username</span>
+                                <span className="label-input100  float-left">Username</span>
                                 <input className="input100" type="text" required={true} value={this.state.Username}
                                        onChange={this.onChange} name="Username" placeholder="daasselk"/>
                                 <span className="focus-input100"/>
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="label-input100">Email</span>
+                                <span className="label-input100  float-left">Email</span>
                                 <input className="input100" type="email" required={true} value={this.state.Email}
                                        onChange={this.onChange} name="Email" placeholder="danielasplund@gmail.com"/>
                                 <span className="focus-input100"/>
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="label-input100">Phone</span>
+                                <span className="label-input100  float-left">Phone</span>
                                 <input className="input100" type="number" required={true} value={this.state.Phone}
                                        onChange={this.onChange} name="Phone" placeholder="0712234567"/>
                                 <span className="focus-input100"/>
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="label-input100">Password</span>
+                                <span className="label-input100  float-left">Password</span>
                                 <input className="input100" type="password" required={true} name="Password"
                                        value={this.state.Password}
                                        onChange={this.onChange}/>
@@ -122,33 +124,31 @@ export default class RegisterPassengerContainer extends Component {
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Name is required">
-                                <span className="label-input100">Confirm Password</span>
+                                <span className="label-input100  float-left">Confirm Password</span>
                                 <input className="input100" type="password" required={true} name="ConfirmPassword"
                                        value={this.state.ConfirmPassword}
                                        onChange={this.onChange}/>
                                 <span className="focus-input100"/>
                             </div>
 
-                            <div className="container-contact100-form-btn">
+                            <div className="container-contact100-form-btn float-left">
                                 <div className="wrap-contact100-form-btn">
                                     <div className="contact100-form-bgbtn"/>
                                     <button className="contact100-form-btn">
 							<span>
-                                Register
-                            <input type="submit" disabled={!this.state.isValid} value=""/>
-								<i className="fa fa-long-arrow-right m-l-7" aria-hidden="true"/>
+                                Register  <i className="fa fa-paper-plane"/>
+                            <input type="submit" value=""/>
 							</span>
                                     </button>
                                 </div>
                             </div>
 
-                            <div className="row mt-5">
-
-                                <div className="col-lg-6 mt-1">
-                                    <button className="btn btn-secondary btn-block" onClick={this.clearForm}>Clear
-                                    </button>
-                                </div>
+                            <div style={{marginTop : "48px", marginLeft : "300px"}}>
+                                <button style={{width : "100px"}} className="btn btn-secondary btn-block" onClick={this.clearForm}>Clear <i
+                                    className="fa fa-eraser"/>
+                                </button>
                             </div>
+
                         </form>
                     </div>
                 </QueueAnim>
