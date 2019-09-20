e8db2ca527e078e163c7146d1e53a55463c0338c
sathiraguruge <33770377+sathiraguruge@users.noreply.github.com>
2019-09-20 09:35:27 +0530

    Spinner Added to select start, end Station

--- project/Release/code/Mobile App/ticketingapp/app/src/main/java/com/neo/ticketingapp/service/RouteService.java
+++ project/Release/code/Mobile App/ticketingapp/app/src/main/java/com/neo/ticketingapp/service/RouteService.java
@@ -1,5 +1,7 @@
 package com.neo.ticketingapp.service;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
@@ -10,4 +12,8 @@ public interface RouteService {
     @Headers("Content-Type: application/json")
     @GET("route/endJourney/{logID}")
     Call<String> endJourney(@Path("logID") String logID);
+
+    @Headers("Content-Type: application/json")
+    @GET("route/getBusHalts/{routeID}")
+    Call<List<String>> getBusHalts(@Path("routeID") String routeID);
 }
